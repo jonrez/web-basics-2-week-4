b27a73c320e675957fb46c1d19113c45f6206ae4
Jon Rezentes <jonrezentes@system76-pc.localdomain>
2021-01-12 12:56:07 -0500

    Finished Assignment

--- index.js
+++ index.js
@@ -3,9 +3,20 @@
 // all right our new element needs style. For that, assign to the new element the className 'square'!. Each time the user clicks on the new element it has to respond changing the displayed text 'x' to 'o'
 
 
-
-
 //resolve // QUESTION 1 here
+const oldDiv = document.getElementById('a-1')
+const newDiv = document.createElement('div')
+const q1Label = document.querySelector('.question-label')
+oldDiv.appendChild(newDiv)
+newDiv.className = 'square'
+
+oldDiv.addEventListener('click', (e) => {
+  const button = e.target
+  if (button.className === 'square' && q1Label.textContent == "Question 1: if you click the square below the text changes 'x' to 'o' or vice versa"){
+    q1Label.textContent = "Question 1: if you click the square below the text changes 'o' to 'x' or vice versa"
+  }else if (button.className === 'square')
+    q1Label.textContent = "Question 1: if you click the square below the text changes 'x' to 'o' or vice versa"
+})
 
 
 
@@ -22,6 +33,21 @@ const colors = [ 'red' , 'white', 'black' , 'green' , 'orange'];
 
 
 //resolve // QUESTION 2 here
+const list = document.getElementById('a-2')
+
+for (let i = 0; i < colors.length; i ++) {
+  const li = document.createElement('li')
+  li.textContent = colors[i]
+  const removeButton = document.createElement('button')
+  removeButton.textContent = 'remove'
+  li.appendChild(removeButton)
+  list.appendChild(li)
+}
+
+list.addEventListener('click', (e) => {
+  li = e.target.parentNode
+  list.removeChild(li)
+})
 
 
 
@@ -34,16 +60,49 @@ const colors = [ 'red' , 'white', 'black' , 'green' , 'orange'];
 //And display the results in the elements with the 'id = volume' and 'id = area' respectively
 //hint: the volumen of a sphere is ((4/3) × π × r^3) and the surface area is (4 × π × r^2)
 
+const measurements = document.getElementById('a-3')
 
 const calculateSphere = () =>{
+  lablesAndInputs = measurements.children
+  const radius = document.getElementById('radius')
+  const volInput = document.getElementById('volume')
+  const surfaceInput = document.getElementById('area')
+  var volume = (4/3) * (parseInt(radius.value)^3) * Math.PI
+  var surfaceArea = 4 * Math.PI * (radius^2)
 
+  if (radius.value > 0){
+    volInput.value = volume
+    surfaceInput.value = surfaceArea
+  }else {
+    volInput.value = 0
+    surfaceInput.value = 0
+  }
  }
 
 
 
 
 //------------------------Question 4 ---------------------------
-//Now in this exercise we want you to create 3 buttons, each with a click action that will hide the respective question's answer above. So if you click the "Hide Question 1" button it will hide from the DOM the answer to your first exercise. If you click it again it will show the answer. 
+//Now in this exercise we want you to create 3 buttons, each with a click action that will hide the respective question's answer above. So if you click the "Hide Question 1" button it will hide from the DOM the answer to your first exercise. If you click it again it will show the answer.
+const buttons = ['1', '2', '3']
+const listOfButtons = document.getElementById('a-4')
 
+for (let i = 0; i < buttons.length; i ++) {
+  const li = document.createElement('button')
+  li.textContent = `hide answere #${buttons[i]}`
+  listOfButtons.appendChild(li)
+}
 
+listOfButtons.addEventListener('click', (e) => {
+  if (e.target.textContent === 'hide answere #1'){
+    oldDiv.removeChild(newDiv)
+    q1Label.textContent = "Question 1: if you click the square below the text changes 'x' to 'o' or vice versa"
+
+  }else if(e.target.textContent === 'hide answere #2'){
+     list.style.display = 'none'
+
+   }else if (e.target.textContent === 'hide answere #3'){
+     measurements.style.display = 'none'
+   }
+})
 //resolve // QUESTION 4 here
